207d6ec8f1ab82a21c7639a93bb3a38bfff0f3ac
Samuel <samuel@127.17.10.128>
2022-10-31 23:57:07 -0700

    1

---
+++ pages/home.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>
+                    هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت
+                </title>
+                <meta property="og:description"
+                      content="گوگل یک به روزرسانی امنیتی جدید برای نسخه مک مرورگر گوگل کروم منتشر کرد."/>
+                <meta property="og:title"
+                      content="هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت"/>
+                <meta property="og:url"
+                      content="https://www.zoomit.ir/software-application/387488-google-high-risk-chrome-security-flaw/"/>
+                <meta property="og:site_name" content="زومیت"/>
+            </Head>
+        </>
+    )
+}
+
+export default Home;

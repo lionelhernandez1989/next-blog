1f77371fe8435436d301307a28ef13b8b1ddb730
Samuel <samuel@127.17.10.128>
2022-11-01 00:05:21 -0700

    3

--- pages/home.js
+++ pages/home.js
@@ -4,16 +4,11 @@ const Home = () => {
     return (
         <>
             <Head>
-                <title>
-                    هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت
-                </title>
-                <meta property="og:description"
-                      content="گوگل یک به روزرسانی امنیتی جدید برای نسخه مک مرورگر گوگل کروم منتشر کرد."/>
-                <meta property="og:title"
-                      content="هشدار گوگل به کاربران مک برای به&zwnj;روزرسانی کروم و رفع مشکل امنیتی این مرورگر  - زومیت"/>
-                <meta property="og:url"
-                      content="https://www.zoomit.ir/software-application/387488-google-high-risk-chrome-security-flaw/"/>
-                <meta property="og:site_name" content="زومیت"/>
+                <title>MSN | Outlook, Office, Skype, Bing, Breaking News, and Latest Videos</title>
+                <meta property="og:description" content="گوگل یک به روزرسانی امنیتی جدید برای نسخه مک مرورگر گوگل کروم منتشر کرد."/>
+                <meta property="og:title" content="MSN | Outlook, Office, Skype, Bing, Breaking News, and Latest Videos"/>
+                <meta property="og:url" content="https://www.msn.com/"/>
+                <meta property="og:image" content="https://static-global-s-msn-com.akamaized.net/hp-neu/sc/c6/519670.jpg"/>
             </Head>
         </>
     )
